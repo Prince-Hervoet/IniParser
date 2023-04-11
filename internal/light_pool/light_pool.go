0e62bf8ff0e7bc6c333baa549d724a6ba0235e37
jammer <1378239869@qq.com>
2023-04-12 01:23:17 +0800

    feat: #create some files

--- internal/light_pool/light_pool.go
+++ internal/light_pool/light_pool.go
@@ -3,8 +3,13 @@ package lightpool
 import (
 	"LightSqlPool/pkg/types"
 	"errors"
+	"sync"
+	"sync/atomic"
 	"time"
 )
+
+const NOT_IN_USE = 0
+const IN_USE = 1
 
 type LightPool struct {
 	firstList          *readList
@@ -15,12 +20,13 @@ type LightPool struct {
 	idleConnections    int32
 	waitTimeout        int64
 	creator            types.ConnectionCreator
+	mu                 *sync.Mutex
 }
 
 type LightConnection struct {
 	pool       *LightPool
 	updateAt   int64
-	isInUse    bool
+	isInUse    int32
 	isRepay    bool
 	Connection interface{}
 }
@@ -63,7 +69,7 @@ func NewLightPool(c *PoolConfig) (*LightPool, error) {
 		lc := &LightConnection{
 			pool:       pool,
 			updateAt:   now,
-			isInUse:    false,
+			isInUse:    NOT_IN_USE,
 			isRepay:    true,
 			Connection: conn,
 		}
@@ -73,8 +79,40 @@ func NewLightPool(c *PoolConfig) (*LightPool, error) {
 	return pool, nil
 }
 
-func (this *LightPool) Get() {
-
+func (this *LightPool) Get() (*LightConnection, error) {
+	for c := 0; c < 2; c++ {
+		for i := int32(0); i < this.firstList.size; i++ {
+			lc := this.firstList.data[i]
+			ok := atomic.CompareAndSwapInt32(&lc.isInUse, NOT_IN_USE, IN_USE)
+			if ok {
+				lc.isRepay = false
+				return lc, nil
+			}
+		}
+	}
+	if this.currentConnections < this.maxConnections {
+		this.mu.Lock()
+		if this.currentConnections < this.maxConnections {
+			defer this.mu.Unlock()
+			conn, err := this.creator.Create()
+			if err != nil {
+				return nil, errors.New("Open connection error")
+			}
+			now := time.Now().UnixMilli()
+			lc := &LightConnection{
+				pool:       this,
+				updateAt:   now,
+				isInUse:    NOT_IN_USE,
+				isRepay:    true,
+				Connection: conn,
+			}
+			this.currentConnections += 1
+			return lc, nil
+		} else {
+			this.mu.Unlock()
+		}
+	}
+	return nil, nil
 }
 
 func (this *LightPool) Repay() {
--- internal/light_pool/read_list.go
+++ internal/light_pool/read_list.go
@@ -7,31 +7,35 @@ import (
 
 const DEFAULT_LIMIT = 1024
 
-type ReadList struct {
+type readList struct {
 	mu    *sync.Mutex
-	data  []interface{}
+	data  []*LightConnection
 	limit int32
 	size  int32
 }
 
-func NewReadList(limit int32) (*ReadList, error) {
+func newReadList(limit int32) (*readList, error) {
 	if limit < 0 {
 		limit = DEFAULT_LIMIT
 	} else if limit == 0 {
 		return nil, errors.New("Limit is 0")
 	}
-	return &ReadList{
+	return &readList{
 		mu:    &sync.Mutex{},
-		data:  make([]interface{}, limit),
+		data:  make([]*LightConnection, limit),
 		limit: limit,
 		size:  0,
 	}, nil
 }
 
-func (this *ReadList) Get(index int32) interface{} {
+func (this *readList) get(index int32) interface{} {
 	return this.data[index]
 }
 
-func (this *ReadList) GetSize() int32 {
+func (this *readList) getSize() int32 {
 	return this.size
+}
+
+func (this *readList) update() {
+
 }
