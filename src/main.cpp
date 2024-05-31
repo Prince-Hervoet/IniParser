2806cbcb95235825d6c4322ee3c30824698d0460
Prince-Hervoet <2064128441@qq.com>
2024-05-31 22:03:41 +0800

    fix: #add codes

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 int main() {
   let_me_see::IniParser ini_parser;
   int a = ini_parser.LoadFile(("./test.ini"));
-  std::string value = ini_parser.GetValue("section1123", "database_url");
+  std::string value = ini_parser.GetValue("section2222", "server_count");
   std::cout << value << std::endl;
   return 0;
 }
